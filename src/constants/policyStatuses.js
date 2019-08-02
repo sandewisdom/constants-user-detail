f9709b348eff4cc5dbd2ce2d9a7abebeb909c30d
sandewisdom <github@sandewisdom.com>
2019-08-02 11:56:50 +0800

    added household type

--- src/constants/policyStatuses.js
+++
@@ -1,8 +0,0 @@
-const policyStatuses = [
-  { name: '群众' },
-  { name: '共青团员' },
-  { name: '中共党员' },
-  { name: '民主党派成员' },
-]
-
-module.exports = policyStatuses
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const genders = require('./constants/genders')
 const ethnics = require('./constants/ethnics')
-const policyStatuses = require('./constants/policyStatuses')
+const policyStatuses = require('./constants/policy-statuses')
+const householdTypes = require('./constants/household-type')
 
 module.exports = {
   genders, 
   ethnics, 
-  policyStatuses 
+  policyStatuses, 
+  householdTypes 
 }
